3e0cae75a2b046bd96c9fb547d84d0ec0594b02f
cpu04 <zf0908@naver.com>
2022-05-01 17:29:04 +0900

    대규모 수정

--- Mainfile.py
+++ Mainfile.py
@@ -1,47 +1,46 @@
-while 1:
-    abc = 0
+def apple(apcoin):
+    app = 0
+    if apcoin == 500:
+        print("사과주스가 나왔습니다. 안녕히 가세요")
+    elif apcoin > 500:
+        print("사과주스가 나왔습니다.")
+        app = apcoin - 500
+        print("거스름돈은 %d원 입니다." % app)
+    else:
+        print("잔액이 부족합니다.")    
+def orange(orcoin):
+    ora = 0
+    if orcoin == 700:
+        print("오렌지 주스가 나왔습니다. 안녕히 가세요")
+    elif orcoin > 700:
+        print("오렌지 주스가 나왔습니다.")
+        ora = orcoin - 700
+        print("거스름돈은 %d원 입니다." % ora)
+    else:
+        print("잔액이 부족합니다.")
+def grape(grcoin):
+    grc = 0
+    if grcoin == 1200:
+        print("포도주스가 나왔습니다. 안녕히 가세요")
+    elif grcoin > 1200:
+        print("포도주스가 나왔습니다.")
+        grc = grcoin - 1200
+        print("거스름돈은 %d원 입니다." % grc)
+    else:
+        print("잔액이 부족합니다.")
 
+
+while(1):
     coin = int(input("금액을 넣어주세요: "))
     print("1. 사과주스 : 500원")
     print("2. 오렌지 주스 : 700원")
     print("3. 포도 주스 : 1200원")
     ans = int(input("음료를 선택해주세요: "))
     if ans == 1:
-        if coin == 500:
-            print("사과주스가 나왔습니다. 안녕히 가세요")
-            continue
-        elif coin > 500:
-            print("사과주스가 나왔습니다.")
-            abc = coin - 500
-            print("거스름돈은 %d원 입니다." % abc)
-            continue
-        else:
-            print("잔액이 부족합니다.")
-            continue
+        apple(coin)
     elif ans == 2:
-        if coin == 700:
-            print("오렌지 주스가 나왔습니다. 안녕히 가세요")
-            continue
-        elif coin > 700:
-            print("오렌지 주스가 나왔습니다.")
-            abc = coin - 700
-            print("거스름돈은 %d원 입니다." % abc)
-            continue
-        else:
-            print("잔액이 부족합니다.")
-            continue
+        orange(coin)
     elif ans == 3:
-        if coin == 1200:
-            print("포도주스가 나왔습니다. 안녕히 가세요")
-            continue
-        elif coin > 1200:
-            print("포도주스가 나왔습니다.")
-            abc = coin - 1200
-            print("거스름돈은 %d원 입니다." % abc)
-            continue
-        else:
-            print("잔액이 부족합니다.")
-            continue
+        grape(coin)
     else:
-        print("잘못된 값입니다.")
-        continue
+        print("잘못된 값입니다.")   
